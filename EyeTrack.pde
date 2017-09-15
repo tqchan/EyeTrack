515bb3b25976926e0f10a47aad0aaf03c20aacf1
tqchan <t.kawada.game@gmail.com>
2017-09-15 13:43:50 +0900

    170915_1343
    鳥1羽
    得点表示

--- EyeTrack.pde
+++ EyeTrack.pde
@@ -8,42 +8,47 @@
 // import org.jorgecardoso.processing.eyetribe.*;
 // import com.theeyetribe.client.data.*;
 
-int COLS = 10;
-int ROWS = 10;
-
 // EyeTribe eyeTribe;
-
 float grid[][];
 PVector point;
-PImage img;
+PImage img,birdImage;
 Bird bird;
-int R,G,B = 255;
+int R,G,B = 0;
+int frame = 60;
+PGraphics pg,birdPg;
+int i;
 
 void setup() {
   fullScreen();
   // size(800, 600);
+  frameRate(frame);
   img = loadImage("data/kumo2.png");
-  
-  smooth();
-  grid = new float[ROWS][COLS];
-  point = new PVector();
+  pg = createGraphics(width, height);
+  pg.beginDraw();
+  pg.image(img,0, 0, width, height);
+  pg.endDraw();
+  // smooth();
+  // point = new PVector();
   // eyeTribe = new EyeTribe(this);
   bird = new Bird();
+  bird.setup();
+
 }
 
 void draw() {
-  background(0);
-  image(img, 0, 0, width, height);
+  // background(0);
+  image(pg, 0, 0);
   noStroke();
   Mouse(mouseX,mouseY);
+  bird.draw();
+  bird.collision();
+  textSize(30);
+  text("" + i, 10, 30);
 }
 
 void Mouse(float mX, float mY){
-  fill(R,G,B,100);
+  fill(R,G,B,255);
   ellipse(mX, mY, 10, 10);
-  // int x = (int)constrain(round(map(mX, 0, width, 0, COLS-1)), 0, COLS-1);
-  // int y = (int)constrain(round(map(mY, 0, height, 0, ROWS-1)), 0, ROWS-1);
-  // grid[y][x] = constrain(grid[y][x]+20, 0, 255);
 }
 
 // void onGazeUpdate(PVector gaze, PVector leftEye_, PVector rightEye_, GazeData data) {
@@ -64,5 +69,37 @@ void Mouse(float mX, float mY){
 
 
 class Bird {
+  int birdX,birdY;
 
+  void setup() {
+    birdImage = loadImage("data/bird.png");
+    birdPg = createGraphics(width/10, height/10);
+    birdX = (int)random(0, width);
+    birdPg.beginDraw();
+    birdPg.image(birdImage, 0, 0);
+    birdPg.endDraw();
+  }
+
+  void draw() {
+    image(birdPg, birdX, birdY);
+    noStroke();
+    birdY += 2;
+    if (birdY == height) {
+      birdX = (int)random(0, width);
+      birdY = 0;
+    }
+  }
+
+  void collision(){
+    int bX,bY,mX,mY;
+    bX = birdX;
+    bY = birdY;
+    mX = mouseX;
+    mY = mouseY;
+    if (bX <= mX && mX <= bX + birdImage.width && bY <= mY && mY <= bY + birdImage.height){
+      birdX = (int)random(0, width);
+      birdY = 0;
+      i++;
+    }
+  }
 }
--- build-tmp/source/EyeTrack.java
+++ build-tmp/source/EyeTrack.java
@@ -24,42 +24,47 @@ public class EyeTrack extends PApplet {
 // import org.jorgecardoso.processing.eyetribe.*;
 // import com.theeyetribe.client.data.*;
 
-int COLS = 10;
-int ROWS = 10;
-
 // EyeTribe eyeTribe;
-
 float grid[][];
 PVector point;
-PImage img;
+PImage img,birdImage;
 Bird bird;
-int R,G,B = 255;
+int R,G,B = 0;
+int frame = 60;
+PGraphics pg,birdPg;
+int i;
 
 public void setup() {
   
   // size(800, 600);
+  frameRate(frame);
   img = loadImage("data/kumo2.png");
-  
-  
-  grid = new float[ROWS][COLS];
-  point = new PVector();
+  pg = createGraphics(width, height);
+  pg.beginDraw();
+  pg.image(img,0, 0, width, height);
+  pg.endDraw();
+  // smooth();
+  // point = new PVector();
   // eyeTribe = new EyeTribe(this);
   bird = new Bird();
+  bird.setup();
+
 }
 
 public void draw() {
-  background(0);
-  image(img, 0, 0, width, height);
+  // background(0);
+  image(pg, 0, 0);
   noStroke();
   Mouse(mouseX,mouseY);
+  bird.draw();
+  bird.collision();
+  textSize(30);
+  text("" + i, 10, 30);
 }
 
 public void Mouse(float mX, float mY){
-  fill(R,G,B,100);
+  fill(R,G,B,255);
   ellipse(mX, mY, 10, 10);
-  // int x = (int)constrain(round(map(mX, 0, width, 0, COLS-1)), 0, COLS-1);
-  // int y = (int)constrain(round(map(mY, 0, height, 0, ROWS-1)), 0, ROWS-1);
-  // grid[y][x] = constrain(grid[y][x]+20, 0, 255);
 }
 
 // void onGazeUpdate(PVector gaze, PVector leftEye_, PVector rightEye_, GazeData data) {
@@ -80,9 +85,41 @@ public void Mouse(float mX, float mY){
 
 
 class Bird {
+  int birdX,birdY;
 
+  public void setup() {
+    birdImage = loadImage("data/bird.png");
+    birdPg = createGraphics(width/10, height/10);
+    birdX = (int)random(0, width);
+    birdPg.beginDraw();
+    birdPg.image(birdImage, 0, 0);
+    birdPg.endDraw();
+  }
+
+  public void draw() {
+    image(birdPg, birdX, birdY);
+    noStroke();
+    birdY += 2;
+    if (birdY == height) {
+      birdX = (int)random(0, width);
+      birdY = 0;
+    }
+  }
+
+  public void collision(){
+    int bX,bY,mX,mY;
+    bX = birdX;
+    bY = birdY;
+    mX = mouseX;
+    mY = mouseY;
+    if (bX <= mX && mX <= bX + birdImage.width && bY <= mY && mY <= bY + birdImage.height){
+      birdX = (int)random(0, width);
+      birdY = 0;
+      i++;
+    }
+  }
 }
-  public void settings() {  fullScreen();  smooth(); }
+  public void settings() {  fullScreen(); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "EyeTrack" };
     if (passedArgs != null) {
